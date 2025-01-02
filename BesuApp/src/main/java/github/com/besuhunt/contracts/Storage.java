a765e99ba90bd0e0177b3d3181a99b07c7beb85e
Giridharan-Govindaraj <mr.giridharangovindharaj@gmail.com>
2025-01-02 20:19:10 +0530

    Generated wrapper for the Storage contract

---
+++ BesuApp/src/main/java/github/com/besuhunt/contracts/Storage.java
@@ -0,0 +1,151 @@
+package github.com.besuhunt.contracts;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.RemoteFunctionCall;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/hyperledger-web3j/web3j/tree/main/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 4.12.3.
+ */
+@SuppressWarnings("rawtypes")
+public class Storage extends Contract {
+    public static final String BINARY = "6080604052348015600e575f80fd5b50335f806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505f600181905550610463806100625f395ff3fe608060405234801561000f575f80fd5b506004361061004a575f3560e01c80633fb5c1cb1461004e578063893d20e81461006a578063a6f9dae114610088578063f2c9ecd8146100a4575b5f80fd5b6100686004803603810190610063919061028f565b6100c2565b005b610072610159565b60405161007f91906102f9565b60405180910390f35b6100a2600480360381019061009d919061033c565b610180565b005b6100ac61024f565b6040516100b99190610376565b60405180910390f35b5f8054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161461014f576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004016101469061040f565b60405180910390fd5b8060018190555050565b5f805f9054906101000a900473ffffffffffffffffffffffffffffffffffffffff16905090565b5f8054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161461020d576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004016102049061040f565b60405180910390fd5b805f806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff16021790555050565b5f600154905090565b5f80fd5b5f819050919050565b61026e8161025c565b8114610278575f80fd5b50565b5f8135905061028981610265565b92915050565b5f602082840312156102a4576102a3610258565b5b5f6102b18482850161027b565b91505092915050565b5f73ffffffffffffffffffffffffffffffffffffffff82169050919050565b5f6102e3826102ba565b9050919050565b6102f3816102d9565b82525050565b5f60208201905061030c5f8301846102ea565b92915050565b61031b816102d9565b8114610325575f80fd5b50565b5f8135905061033681610312565b92915050565b5f6020828403121561035157610350610258565b5b5f61035e84828501610328565b91505092915050565b6103708161025c565b82525050565b5f6020820190506103895f830184610367565b92915050565b5f82825260208201905092915050565b7f6f6e6c79206f776e65722063616e206368616e676520746865206f776e6572735f8201527f6869700000000000000000000000000000000000000000000000000000000000602082015250565b5f6103f960238361038f565b91506104048261039f565b604082019050919050565b5f6020820190508181035f830152610426816103ed565b905091905056fea26469706673582212209488995bf5a6cee42fb7e2e4f2682fa2b8ab5c98f70555858d0a611f9338f00364736f6c634300081a0033";
+
+    private static String librariesLinkedBinary;
+
+    public static final String FUNC_CHANGEOWNER = "changeOwner";
+
+    public static final String FUNC_GETNUMBER = "getNumber";
+
+    public static final String FUNC_GETOWNER = "getOwner";
+
+    public static final String FUNC_SETNUMBER = "setNumber";
+
+    @Deprecated
+    protected Storage(String contractAddress, Web3j web3j, Credentials credentials,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected Storage(String contractAddress, Web3j web3j, Credentials credentials,
+            ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected Storage(String contractAddress, Web3j web3j, TransactionManager transactionManager,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected Storage(String contractAddress, Web3j web3j, TransactionManager transactionManager,
+            ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> changeOwner(String _owner) {
+        final Function function = new Function(
+                FUNC_CHANGEOWNER, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(160, _owner)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteFunctionCall<BigInteger> getNumber() {
+        final Function function = new Function(FUNC_GETNUMBER, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public RemoteFunctionCall<String> getOwner() {
+        final Function function = new Function(FUNC_GETOWNER, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> setNumber(BigInteger _number) {
+        final Function function = new Function(
+                FUNC_SETNUMBER, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(_number)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    @Deprecated
+    public static Storage load(String contractAddress, Web3j web3j, Credentials credentials,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        return new Storage(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static Storage load(String contractAddress, Web3j web3j,
+            TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Storage(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static Storage load(String contractAddress, Web3j web3j, Credentials credentials,
+            ContractGasProvider contractGasProvider) {
+        return new Storage(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static Storage load(String contractAddress, Web3j web3j,
+            TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new Storage(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<Storage> deploy(Web3j web3j, Credentials credentials,
+            ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(Storage.class, web3j, credentials, contractGasProvider, getDeploymentBinary(), "");
+    }
+
+    public static RemoteCall<Storage> deploy(Web3j web3j, TransactionManager transactionManager,
+            ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(Storage.class, web3j, transactionManager, contractGasProvider, getDeploymentBinary(), "");
+    }
+
+    @Deprecated
+    public static RemoteCall<Storage> deploy(Web3j web3j, Credentials credentials,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Storage.class, web3j, credentials, gasPrice, gasLimit, getDeploymentBinary(), "");
+    }
+
+    @Deprecated
+    public static RemoteCall<Storage> deploy(Web3j web3j, TransactionManager transactionManager,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Storage.class, web3j, transactionManager, gasPrice, gasLimit, getDeploymentBinary(), "");
+    }
+
+    public static void linkLibraries(List<Contract.LinkReference> references) {
+        librariesLinkedBinary = linkBinaryWithReferences(BINARY, references);
+    }
+
+    private static String getDeploymentBinary() {
+        if (librariesLinkedBinary != null) {
+            return librariesLinkedBinary;
+        } else {
+            return BINARY;
+        }
+    }
+}
